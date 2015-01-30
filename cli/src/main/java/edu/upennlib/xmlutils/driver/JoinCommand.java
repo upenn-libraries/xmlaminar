3d4f3394e9177e124413531048a0a03718588b5d
Michael Gibney <michael@michaelgibney.net>
2015-01-30 12:53:13 -0500

    removed old output dependencies in JoinCommand

--- cli/src/main/java/edu/upennlib/xmlutils/driver/JoinCommand.java
+++ cli/src/main/java/edu/upennlib/xmlutils/driver/JoinCommand.java
@@ -18,24 +18,16 @@ package edu.upennlib.xmlutils.driver;
 
 import edu.upennlib.paralleltransformer.JoiningXMLFilter;
 import edu.upennlib.paralleltransformer.QueueSourceXMLFilter;
-import edu.upennlib.paralleltransformer.SerializingXMLFilter;
-import edu.upennlib.paralleltransformer.callback.BaseRelativeFileCallback;
-import edu.upennlib.paralleltransformer.callback.StaticFileCallback;
-import edu.upennlib.paralleltransformer.callback.StdoutCallback;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Collections;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerFactory;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
-import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLFilter;
 
@@ -45,14 +37,9 @@ import org.xml.sax.XMLFilter;
  */
 public class JoinCommand implements Command<InputCommandFactory.InputCommand> {
 
-    private static final TransformerFactory tf;
     protected InputCommandFactory.InputCommand inputBase;
-    protected File output;
-    protected OptionSpec<File> outputFileSpec;
     protected OptionSpec joinAllSpec;
     protected boolean joinAll;
-    protected boolean noIndent;
-    protected OptionSpec noIndentSpec;
 
     protected OptionSpec verboseSpec;
     protected OptionSpec helpSpec;
@@ -62,20 +49,10 @@ public class JoinCommand implements Command<InputCommandFactory.InputCommand> {
     protected final boolean first;
     protected final boolean last;
     
-    static {
-        tf = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null);
-    }
-    
     protected JoinCommand(boolean first, boolean last) {
         this.first = first;
         this.last = last;
         parser = new OptionParser();
-        if (last) {
-            outputFileSpec = parser.acceptsAll(Flags.OUTPUT_FILE_ARG, "output")
-                    .withRequiredArg().ofType(File.class)
-                    .describedAs("'-' for stdout");
-            noIndentSpec = parser.acceptsAll(Flags.NO_INDENT_ARG, "prevent default indenting of output");
-        }
         joinAllSpec = parser.acceptsAll(Flags.JOIN_ALL_ARG, "join all output, irrespective of input systemId");
         verboseSpec = parser.acceptsAll(Flags.VERBOSE_ARG, "be more verbose");
         helpSpec = parser.acceptsAll(Flags.HELP_ARG, "show help").forHelp();
@@ -95,14 +72,6 @@ public class JoinCommand implements Command<InputCommandFactory.InputCommand> {
             return false;
         }
         joinAll = options.has(joinAllSpec);
-        if (last) {
-            if (options.has(outputFileSpec)) {
-                output = options.valueOf(outputFileSpec);
-            } else {
-                output = new File("-");
-            }
-            noIndent = options.has(noIndentSpec);
-        }
         return true;
     }
 
@@ -137,34 +106,7 @@ public class JoinCommand implements Command<InputCommandFactory.InputCommand> {
         if (first && inputBase.filesFrom != null) {
             joiner.setInputType(QueueSourceXMLFilter.InputType.indirect);
         }
-        if (true || !last) {
-            return joiner;
-        } else {
-            if (true) {
-                throw new AssertionError("XXX");
-            }
-            if (joinAll) {
-                SerializingXMLFilter serializer = new SerializingXMLFilter(output);
-                serializer.setParent(noIndent ? joiner : new OutputTransformerConfigurer(joiner, Collections.singletonMap("indent", "yes")));
-                return serializer;
-            } else {
-                Transformer t;
-                try {
-                    t = tf.newTransformer();
-                } catch (TransformerConfigurationException ex) {
-                    throw new RuntimeException(ex);
-                }
-                XMLFilter outputConfigurer = noIndent ? null : new OutputTransformerConfigurer(Collections.singletonMap("indent", "yes"));
-                if ("-".equals(output.getPath())) {
-                    joiner.setOutputCallback(new StdoutCallback(t, outputConfigurer));
-                } else if (output.isDirectory()) {
-                    joiner.setOutputCallback(new BaseRelativeFileCallback(inputBase.getInputBase(), output, t));
-                } else {
-                    joiner.setOutputCallback(new StaticFileCallback(t, output, outputConfigurer));
-                }
-                return joiner;
-            }
-        }
+        return joiner;
     }
 
     @Override
